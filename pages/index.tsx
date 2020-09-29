e6f564344b748cb48c3a8380cb81c3f7985c61e3
Claus Rodrigues Tessmann <clausrt@outlook.com>
2020-09-28 22:39:20 -0300

    Config. básica.

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import React, { FunctionComponent } from 'react'
+
+const Home: FunctionComponent = () => {
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+      <main>
+        <h1>Homepage Component</h1>
+      </main>
+    </>
+  )
+}
+
+export default Home
